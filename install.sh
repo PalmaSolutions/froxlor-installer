46e3dc88aca665452262875fbff3469ddc461cc0
xWuZaa <info@ichwillne.info>
2015-02-11 16:58:35 +0100

    Language selection and info screen

--- install.sh
+++ install.sh
@@ -7,9 +7,14 @@
 # 2015-02-11
 ### END INFO
 
+out="/tmp/$$"
+
 dialog --title "Froxlor installer" \
 	--menu "Please select your language:" 10 40 5 \
-	"Deutsch" "de" \
-	"English" "en"
+	"de" "Deutsch" \
+	"en" "English" 2>$out
 
-echo $?
+lang=$(<$out)
+
+dialog --title "Froxlor installer" \
+	--msgbox "$(cat include/lang/$lang/info.inc)" 20 50
